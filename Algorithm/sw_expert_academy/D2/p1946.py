3978c841af826ec3a4b8243e9ff177823038cf7c
TaeJuneJoung <jtj0525@gmail.com>
2019-06-05 19:59:54 +0900

    re-leaning_D2

--- Algorithm/sw_expert_academy/D2/p1946.py
+++ Algorithm/sw_expert_academy/D2/p1946.py
@@ -1,22 +1,27 @@
-T = int(input())
-for t in range(1,T+1):
-    N = int(input())
-    print("#"+str(t))
+"""
+[1946.간단한 압축 풀기]
+10개가 찍히면 다음줄로 내려가는 처리가 중요한 문제
+또한, 테스트케이스가 하나 끝나면 한줄을 내려줘야한다
 
-    S = [0 for i in range(N)]
-    M = [0 for i in range(N)]
-    cnt = 0
-    line = 0
+T : 테스트케이스
+N : 받는 갯수
+sum_num : 10개씩 띄어주기 위해서 사용
+value : string타입의 값
+num : value가 나오는 횟수
+"""
+
+T = int(input())
+for t in range(1, T+1):
+    print("#{}".format(t))
+    N = int(input())
+    sum_num = 0
     for i in range(N):
-        S[i], temp = map(str, input().split())
-        M[i] = int(temp)
-    
-    cnt = 0
-    for i in range(N):
-        for j in range(M[i]):
-            cnt += 1
-            print(S[i], end="")
-            if cnt == 10:
-                cnt -= 10
+        value, num = map(str, input().split())
+        num = int(num)
+        for j in range(num):
+            print(value, end="")
+            sum_num += 1
+            if sum_num == 10:
+                sum_num = 0
                 print()
     print()
